7b186790b45ac019e3ac94d3ebe8554b218153df
Felix-Rojas <A01201946@tec.com>
2022-06-27 12:22:52 -0500

    done

--- ListaTemplate.h
+++ ListaTemplate.h
@@ -1,5 +1,5 @@
-#ifndef _LISTA_H_
-#define _LISTA_H_
+#ifndef _LISTA_TEMPLATE_H_
+#define _LISTA_TEMPLATE_H_
 
 #include <iostream>
 #include <vector>
@@ -7,45 +7,71 @@
 const int MAX = 100;
 
 template <class T>
-class ListaTemplate {
-  private:
+class ListaTemplate
+{
+private:
     std::vector<T> data;
     int currentSize;
 
-  public:
+public:
     ListaTemplate();
     bool insert(T value);
     void erase();
-    T getData(int indice);
+    auto getData(int indice);
     int getSize();
     void print();
 };
 
-template<class T> 
-ListaTemplate<T>::ListaTemplate() {
-  currentSize = 0;
+template <class T>
+ListaTemplate<T>::ListaTemplate()
+{
+    currentSize = 0;
 }
 
-template<class T>
-bool ListaTemplate<T>::insert(T value) {
-  if (currentSize < MAX) {
-    data.push_back(value);
-    currentSize++;
-    return true;
-  }
-  else 
-    return false;
+template <class T>
+bool ListaTemplate<T>::insert(T value)
+{
+    if (currentSize < MAX)
+    {
+        data.push_back(value);
+        currentSize++;
+        return true;
+    }
+    else
+        return false;
 }
 
-template<class T>
-void ListaTemplate<T>::erase() {
-  if (currentSize > 0) {
-    std::cout << getData(currentSize-1) << std::endl;
-    data.pop_back();
-    currentSize--;
-  }
-  else
-    std::cout << "NO HAY ELEMENTOS" << std::endl;
+template <class T>
+void ListaTemplate<T>::erase()
+{
+    if (currentSize > 0)
+    {
+        std::cout << getData(currentSize - 1) << std::endl;
+        data.pop_back();
+        currentSize--;
+    }
+    else
+        std::cout << "NO HAY ELEMENTOS" << std::endl;
 }
 
-#endif // _LISTA_H_
+template <class T>
+    auto ListaTemplate<T>::getData(const int indice)
+    {
+        return data.at(indice);
+    }
+
+template <class T>
+    int ListaTemplate<T>::getSize(){
+        return currentSize;
+    }
+
+template <class T>
+    void ListaTemplate<T>::print(){
+        int pos = 0;
+        for (auto i: data) {
+            std::printf("[%d] %d \n", pos,i);
+            pos+=1;
+        }
+    }
+
+#endif //_LISTA_TEMPLATE_H_
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-  compilar: 
+  compilar:
     g++ -std=c++17 -Wall *.cpp
 
   ejecutar:
@@ -9,11 +9,28 @@
 #include <iostream>
 #include "ListaTemplate.h"
 
-int main() {
-  std::cout << "Template example!\n";
-  ListaTemplate<int> myList;
+int main()
+{
+    std::cout << "Template example!\n";
+    ListaTemplate<int> myList;
 
-  myList.insert(15);
-  
-  return 0;
+    myList.insert(15);
+    myList.insert(1);
+    myList.insert(13);
+    myList.insert(14);
+    myList.print();
+    std::cout << std::endl;
+
+    std::cout << myList.getData(3) << std::endl;
+    myList.erase();
+    myList.print();
+    std::cout << std::endl;
+
+    std::cout << "Elementos en vector: "<< myList.getSize() << std::endl;
+    myList.erase();
+    myList.erase();
+    myList.erase();
+    myList.erase();
+
+    return 0;
 }
